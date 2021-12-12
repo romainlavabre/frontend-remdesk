16f63a3e6a62b5e00e2ba4d7544d940ff57f0b8e
romainlavabre <romainlavabre98@gmail.com>
2021-12-12 02:28:56 +0100

    ADD Database configuration form

--- src/components/home/DatabaseConfiguration.vue
+++ src/components/home/DatabaseConfiguration.vue
@@ -14,15 +14,32 @@
                     {{ $message.ACCESS_ALLOWED }}
                 </v-card-actions>
                 <v-card-text>
-                    <v-form>
+                    <Loading v-if="formLoading"/>
+                    <v-form @submit.prevent="send()" v-if="!formLoading">
                         <v-row>
                             <v-col cols="6">
-                                <v-text-field label="IP du serveur de base de donnée"></v-text-field>
-                                <v-text-field label="Port"></v-text-field>
+                                <v-select label="Serveur"
+                                          :items="[{text: 'MYSQL', value: 'mysql', selected: true},{text: 'POSTGRESQL', value: 'postgresql'}]"
+                                          v-model="form.software"></v-select>
+                                <v-text-field v-model="form.host"
+                                              label="IP du serveur de base de donnée"></v-text-field>
+                                <v-text-field v-model="form.port" label="Port"></v-text-field>
                             </v-col>
                             <v-col cols="6">
-                                <v-text-field type="password" label="Nom d'utilisateur"></v-text-field>
-                                <v-text-field type="password" label="Mot de passe"></v-text-field>
+                                <v-text-field v-model="form.username" type="password"
+                                              label="Nom d'utilisateur"></v-text-field>
+                                <v-text-field v-model="form.password" type="password"
+                                              label="Mot de passe"></v-text-field>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col cols="12" class="text-right">
+                                <v-btn type="submit" color="green" v-if="!configured">
+                                    Configurer
+                                </v-btn>
+                                <v-btn type="submit" color="orange" v-if="configured">
+                                    Mettre à jour
+                                </v-btn>
                             </v-col>
                         </v-row>
                     </v-form>
@@ -33,11 +50,88 @@
 </template>
 
 <script>
+import Loading from "@/components/util/Loading";
+
 export default {
     name: "DatabaseConfiguration",
+    components: {Loading},
     data() {
         return {
-            configured: this.$storage.access.databaseSet
+            configured: this.$storage.access.databaseSet,
+            formLoading: false,
+            form: {
+                software: null,
+                host: null,
+                port: null,
+                username: null,
+                password: null
+            }
+        }
+    },
+    methods: {
+        send() {
+            this.formLoading = true;
+
+            this.$http
+                .post(process.env.VUE_APP_BACKEND_URL + "/guest/configuration/database", {
+                    database: this.form
+                })
+                .then((response) => {
+                    this.$root.$emit(this.$event.SYSTEM_ALERT, {
+                        text: "Base de données configurée",
+                        type: 'success'
+                    });
+
+                    this.rebootBackend();
+                })
+                .catch((error) => {
+                    this.$root.$emit(this.$event.SYSTEM_ALERT, {
+                        text: this.$message.select(error.response.data.message),
+                        type: 'warning'
+                    });
+                    this.formLoading = false;
+                });
+        },
+        async rebootBackend() {
+            this.$http
+                .post(process.env.VUE_APP_BACKEND_URL + "/guest/system/reboot")
+                .then((response) => {
+                    this.$root.$emit(this.$event.SYSTEM_ALERT, {
+                        text: "Veuillez patienter, nous redémarrons le système ...",
+                        type: 'info',
+                        duration: 5000
+                    });
+                })
+                .catch((error) => {
+                    this.$root.$emit(this.$event.SYSTEM_ALERT, {
+                        text: "Une erreur s'est produite lors du redémarrage du système, nous vous conseillons de redémarrer Remdesk",
+                        type: 'error'
+                    });
+                });
+
+            let rebooted = false;
+
+            while (!rebooted) {
+                await this.sleep(1000);
+
+                this.$http
+                    .get(process.env.VUE_APP_BACKEND_URL + "/guest/system/ping")
+                    .then((response) => {
+                        rebooted = true;
+                    })
+                    .catch((error) => {
+                    });
+            }
+
+            this.$root.$emit(this.$event.SYSTEM_ALERT, {
+                text: "Système redémarré",
+                type: 'green'
+            });
+            this.formLoading = false;
+
+        },
+        sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
         }
     },
     mounted() {
--- src/plugins/message/index.js
+++ src/plugins/message/index.js
@@ -1,6 +1,23 @@
-
 export default {
     FILE_ACCESS_NOT_ALLOWED: "Configurez les accès au stockage de fichier",
     DATABASE_ACCESS_NOT_ALLOWED: "Configurez les accès à la base de données",
-    ACCESS_ALLOWED: "Accès configurés"
+    ACCESS_ALLOWED: "Accès configurés",
+    backendMessages: {
+        DATABASE_CONFIGURATION_NO_CONNECTION_ESTABLISHED: "Impossible d'établir une connexion à la base de données",
+        DATABASE_CONFIGURATION_SOFTWARE_REQUIRED: "Serveur requis",
+        DATABASE_CONFIGURATION_HOST_REQUIRED: "IP requise",
+        DATABASE_CONFIGURATION_USERNAME_REQUIRED: "Nom d'utilisateur requis",
+        DATABASE_CONFIGURATION_PORT_REQUIRED: "Port requis"
+    },
+    select: function (error) {
+        for (const key in this.backendMessages) {
+            console.log(key)
+            console.log(error)
+            if (key === error) {
+                return this.backendMessages[key];
+            }
+        }
+
+        return "Erreur inconnue";
+    }
 }
