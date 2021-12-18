d3d1d7e23d855b24e6fe8e1e374fd26598876038
romainlavabre <romainlavabre98@gmail.com>
2021-12-18 22:03:29 +0100

    ADD FileSoftwareUsage configuration form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
         <v-row>
             <v-col cols="6">
                 <Presentation/>
+                <FileSoftwareUsageConfiguration/>
             </v-col>
             <v-col cols="6">
                 <DatabaseConfiguration/>
@@ -16,10 +17,12 @@
 import Presentation from '../components/home/Presentation.vue';
 import DatabaseConfiguration from "@/components/home/DatabaseConfiguration";
 import FileStorageConfiguration from "@/components/home/FileStorageConfiguration";
+import FileSoftwareUsageConfiguration from "@/components/home/FileSoftwareUsageConfiguration";
 
 export default {
     name: 'Home',
     components: {
+        FileSoftwareUsageConfiguration,
         FileStorageConfiguration,
         DatabaseConfiguration,
         Presentation,
