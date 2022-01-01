5d7ae434930d5deb1c7db84f47844ddc5fee0fe3
romainlavabre <romainlavabre98@gmail.com>
2022-01-01 18:04:25 +0100

    FIX linux/compile.sh

--- compile/linux/compile.sh
+++ compile/linux/compile.sh
@@ -3,9 +3,9 @@
 BACKEND_DIR=/var/www/remdesk
 FRONTEND_DIR=/var/vue/remdesk-frontend
 
-#cd "$BACKEND_DIR"
-#./mvnw clean package -Dmaven.test.skip=true --no-transfer-progress
-#cp target/api-0.0.1-SNAPSHOT.jar "$FRONTEND_DIR/app.jar"
+cd "$BACKEND_DIR"
+./mvnw clean package -Dmaven.test.skip=true --no-transfer-progress
+cp target/api-0.0.1-SNAPSHOT.jar "$FRONTEND_DIR/app.jar"
 
 cd "$FRONTEND_DIR"
 #yarn build
